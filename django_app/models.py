dc03d46c597565e404e531e8566dc53013db53b3
CodeSpeak <gen@codespeak.dev>
2025-10-18 12:45:59 +0000

    Run: df4d0f46-2556-44bc-9fcc-cf8d017a388e, Phase: ImplementSpecWithClaudeCode

--- django_app/models.py
+++ django_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Todo(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    marked_as_done_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.content[:50]
+
+    @property
+    def is_done(self):
+        return self.marked_as_done_at is not None
--- django_proj/settings.py
+++ django_proj/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l(tvjq@l39d!p790n-+&l45efk%ly5wo-++35&i-**_t1s8hr9'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-l(tvjq@l39d!p790n-+&l45efk%ly5wo-++35&i-**_t1s8hr9')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',') if os.environ.get('ALLOWED_HOSTS') else []
+
+# CSRF trusted origins
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',') if os.environ.get('CSRF_TRUSTED_ORIGINS') else []
 
 
 # Application definition
@@ -73,10 +78,7 @@ WSGI_APPLICATION = 'django_proj.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=f'sqlite:///{BASE_DIR / "db.sqlite3"}')
 }
 
 
